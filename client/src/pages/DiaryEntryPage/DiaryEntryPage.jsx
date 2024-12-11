a9ef021cf804aa342764f5e439f33c2bce2d7c37
Reena Sewraz <reenasewraz@gmail.com>
2024-12-11 15:13:44 +0000

    diary entry frontend tweaks plus starter files for backend form

---
+++ client/src/pages/DiaryEntryPage/DiaryEntryPage.jsx
@@ -0,0 +1,12 @@
+import DiaryEntryForm from "../../components/DiaryEntry"
+
+export function DiaryEntryPage() {
+
+    return (
+
+        <div className="diary-entry-page">
+        <h1>Add a new diary entry</h1>
+        <DiaryEntryForm />
+        </div>
+);
+}
---
+++ client/src/services/diary_entry.js
@@ -0,0 +1,21 @@
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+export async function CreateEntry(token, body) {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ message: body }),
+    };
+  
+    const response = await fetch(`${BACKEND_URL}/diary`, requestOptions);
+  
+    if (response.status !== 201) {
+      throw new Error("Unable to create diary entry");
+    }
+  
+    return response;
+  }
+  
