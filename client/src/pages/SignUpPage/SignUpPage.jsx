f24ee73545587a804ecc6be3a315465f91418b92
Naomi <naomismith97@yahoo.co.uk>
2024-12-11 11:51:57 +0000

    client controller and route, server page and css

---
+++ client/src/pages/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { submitNewUser } from "../../services/users.js";
+import "./SignUpPage.css"
+
+export function SignUpPage() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [errorMessage, setErrorMessage] = useState("");
+    const navigate = useNavigate();
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            setErrorMessage("");
+
+            const formData = new FormData();
+            formData.append("name", name);
+            formData.append("email", email);
+            formData.append("password", password);
+
+            const data = await submitNewUser(formData);
+
+            console.log("Signup successful:", data);
+            navigate("/login");
+        } catch (error) {
+            console.error("Error during signup:", error);
+            setErrorMessage(error.message);
+        }
+    };
+
+    return (
+        <div>
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
+            <form className="sign-up-form" onSubmit={handleSubmit}>
+                <h2>Signup</h2>
+                <label htmlFor="name">Name:</label>
+                <input
+                    id="name"
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                />
+                <br />
+                <br />
+                <label htmlFor="email">Email:</label>
+                <input
+                    id="email"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <br />
+                <br />
+                <label htmlFor="password">Password:</label>
+                <input
+                    id="password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    minLength={4}
+                />
+                <br />
+                <br />
+                <button type="submit">Sign Up Now!</button>
+            </form>
+        </div>
+    );
+};
