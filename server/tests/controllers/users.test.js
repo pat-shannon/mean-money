43c6675932b0e7f008840ba856cc8bd319672306
Patrick Shannon <shannon.patrick06@gmail.com>
2024-12-20 12:20:16 +0000

    Added partial user controller testing

--- server/tests/controllers/users.test.js
+++ server/tests/controllers/users.test.js
@@ -2,6 +2,7 @@ const request = require("supertest");
 
 const app = require("../../app");
 const User = require("../../models/user");
+const bcrypt = require('bcrypt');
 
 require("../mongodb_helper");
 
@@ -30,10 +31,36 @@ describe("/users", () => {
             expect(newUser.email).toEqual("thisisanemail@email.com");
             expect(bcrypt.compare(newUser.password, '1234Password?'));
         });
+
+        test("When a password exactly 8 characters long is provided with a capital letter, a number, and a special character, response code is 201 and user is saved", async () => {
+            const response = await request(app)
+                .post("/users")
+                .send({ name: "Emailer", email: "thisisanemail@email.com", password: "Mypass1?" });
+
+            expect(response.statusCode).toBe(201);
+            const users = await User.find();
+            const newUser = users[users.length - 1];
+            expect(newUser.name).toEqual("Emailer");
+            expect(newUser.email).toEqual("thisisanemail@email.com");
+            expect(bcrypt.compare(newUser.password, 'Mypass1?'));
+        });
+
+        test("When a password over 8 characters long is provided with a capital letter, a number, and a special character, response code is 201 and user is saved", async () => {
+            const response = await request(app)
+                .post("/users")
+                .send({ name: "Emailer", email: "thisisanemail@email.com", password: "Mypass1?" });
+
+            expect(response.statusCode).toBe(201);
+            const users = await User.find();
+            const newUser = users[users.length - 1];
+            expect(newUser.name).toEqual("Emailer");
+            expect(newUser.email).toEqual("thisisanemail@email.com");
+            expect(bcrypt.compare(newUser.password, 'Mypassword123? '));
+        });
     });
 
-    describe("POST, when password is missing", () => {
-        test("response code is 400", async () => {
+    describe("POST, when valid password not provided", () => {
+        test("When no password is provided, response code is 400", async () => {
             const response = await request(app)
                 .post("/users")
                 .send({ email: "emailtwo@email.com" });
@@ -41,28 +68,152 @@ describe("/users", () => {
             expect(response.statusCode).toBe(400);
         });
 
-        test("does not create a user", async () => {
+        test("When no password is provided, does not create a user", async () => {
             await request(app).post("/users").send({ email: "emailtwo@email.com" });
 
             const users = await User.find();
             expect(users.length).toEqual(0);
         });
-    });
 
-    describe("POST, when email is missing", () => {
-        test("response code is 400", async () => {
+        test("When a password under 8 characters is provided, response code is 400", async () => {
             const response = await request(app)
                 .post("/users")
-                .send({ password: "1234password" });
+                .send({ name: "Emailer", email: "thisisanemail@email.com", password: "Seven7?" });
 
             expect(response.statusCode).toBe(400);
         });
 
-        test("does not create a user", async () => {
-            await request(app).post("/users").send({ password: "1234password" });
+        test("When a password over 7 characters is provided with no capital letter, response code is 400", async () => {
+            const response = await request(app)
+                .post("/users")
+                .send({ name: "Emailer", email: "thisisanemail@email.com", password: "password123!" });
 
+            expect(response.statusCode).toBe(400);
+        });
+        test("When a password over 7 characters is provided with no number, response code is 400", async () => {
+            const response = await request(app)
+                .post("/users")
+                .send({ name: "Emailer", email: "thisisanemail@email.com", password: "Password!" });
+
+            expect(response.statusCode).toBe(400);
+        });
+        test("When a password over 7 characters is provided with no special character, response code is 400", async () => {
+            const response = await request(app)
+                .post("/users")
+                .send({ name: "Emailer", email: "thisisanemail@email.com", password: "Password123" });
+
+            expect(response.statusCode).toBe(400);
+        });
+        
+    });
+
+    describe("POST, when valid email not provided", () => {
+        test("missing email means response code is 400, account not created", async () => {
+            const response = await request(app)
+                .post("/users")
+                .send({ name: "Emailer", password: "Mypass1?" });
+
+            expect(response.statusCode).toBe(400);
             const users = await User.find();
             expect(users.length).toEqual(0);
+        });
+
+        test("email with no domain means response code is 500, account not created", async () => {
+            const response = await request(app)
+                .post("/users")
+                .send({ name: "Emailer", email: "emailaddress", password: "Mypass1?" });
+
+            expect(response.statusCode).toBe(500);
+            const users = await User.find();
+            expect(users.length).toEqual(0);
+        });
+
+        test("email with @ sign but no domain means response code is 500, account not created", async () => {
+            const response = await request(app)
+                .post("/users")
+                .send({ name: "Emailer", email:"emailaddress@", password: "Mypass1?" });
+
+            expect(response.statusCode).toBe(500);
+            const users = await User.find();
+            expect(users.length).toEqual(0);
+        });
+
+        test("email with . sign but no @ sign means response code is 500, account not created", async () => {
+            const response = await request(app)
+                .post("/users")
+                .send({ name: "Emailer", email:"emailaddresswebsite.com", password: "Mypass1?" });
+
+            expect(response.statusCode).toBe(500);
+            const users = await User.find();
+            expect(users.length).toEqual(0);
+        });
+
+        test("email with @ sign but no domain means response code is 500, account not created", async () => {
+            const response = await request(app)
+                .post("/users")
+                .send({ name: "Emailer", email:"emailaddress@", password: "Mypass1?" });
+
+            expect(response.statusCode).toBe(500);
+            const users = await User.find();
+            expect(users.length).toEqual(0);
+        });
+
+        test("email with @ and . sign but wrong order means response code is 500, account not created", async () => {
+            const response = await request(app)
+                .post("/users")
+                .send({ name: "Emailer", email:"emailaddress.@com", password: "Mypass1?" });
+
+            expect(response.statusCode).toBe(500);
+            const users = await User.find();
+            expect(users.length).toEqual(0);
+        });
+
+        test("email with @ and . sign in correct order but no domain means response code is 500, account not created", async () => {
+            const response = await request(app)
+                .post("/users")
+                .send({ name: "Emailer", email:"emailaddress@.", password: "Mypass1?" });
+
+            expect(response.statusCode).toBe(500);
+            const users = await User.find();
+            expect(users.length).toEqual(0);
+        });
+
+        test("email with @ and . sign in correct order but no top-level domain means response code is 500, account not created", async () => {
+            const response = await request(app)
+                .post("/users")
+                .send({ name: "Emailer", email:"emailaddress@website.", password: "Mypass1?" });
+
+            expect(response.statusCode).toBe(500);
+            const users = await User.find();
+            expect(users.length).toEqual(0);
+        });
+
+        test("email with @ and . sign in correct order with only top-level domain means response code is 500, account not created", async () => {
+            const response = await request(app)
+                .post("/users")
+                .send({ name: "Emailer", email:"emailaddress@.com", password: "Mypass1?" });
+
+            expect(response.statusCode).toBe(500);
+            const users = await User.find();
+            expect(users.length).toEqual(0);
+        });
+
+
+    });
+
+    describe("POST, when email already in use provided", () => {
+        test("returns 400, user not created", async () => {
+            await request(app)
+                .post("/users")
+                .send({ name: "Emailer", email:"emailaddress@website.net", password: "Mypass1?" });
+
+            const response = await request(app)
+                .post("/users")
+                .send({ name: "Emailer", email:"emailaddress@website.net", password: "Mypass1?" });
+
+            expect(response.statusCode).toBe(400);
+            const users = await User.find();
+            expect(users.length).toEqual(1);
         });
     });
 });
